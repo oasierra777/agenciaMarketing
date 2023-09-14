e8a761256492018553c32f1c38efb40f01ffd1d9
oasierra777 <oasierra777@gmail.com>
2023-09-14 14:14:34 -0500

    Termine la pagina de nosotros

--- src/containers/pages/About.jsx
+++ src/containers/pages/About.jsx
@@ -3,6 +3,12 @@ import { useEffect } from "react"
 import Footer from "components/navigation/Footer"
 import Navbar from "components/navigation/Navbar"
 import Layout from "hocs/layouts/Layout"
+import Header from "components/About/Header"
+import TestStats from "components/About/TestSatats"
+import Images from "components/About/Image"
+import Clients from "components/About/Clients"
+import Features from "components/About/Features"
+import Team from "components/About/Teams"
 
 function About(){
 
@@ -14,7 +20,12 @@ function About(){
         <Layout>
             <Navbar/>
             <div className="pt-28">
-                About
+                <Header/>
+                <TestStats/>
+                <Images/>
+                <Clients/>
+                <Features/>
+                <Team/>
             </div>
             <Footer/>
         </Layout>
