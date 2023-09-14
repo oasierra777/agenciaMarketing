22dfea50767467f0f5caf6d117a141f5a02df800
oasierra777 <oasierra777@gmail.com>
2023-09-14 15:57:18 -0500

    Se refinan detalles para mejorar la experiencia de usuario

--- src/components/services/Header.js
+++ src/components/services/Header.js
@@ -5,7 +5,7 @@ function Header(){
   return(
     <main>
       <div className="relative px-6 lg:px-8">
-        <div className="mx-auto max-w-full xl:mx-12 xl:pt-40 xl:pb-64 lg:pt-40 lg:pb-48 pt-24 pb-12">
+        <div className="mx-auto lg:mx-12 max-w-full xl:mx-12 xl:pt-40 xl:pb-64 lg:pt-40 lg:pb-48 pt-24 pb-12">
           <div>
             <div>
               <h1 className="text-4xl font-semibold tracking-tight pb-16 sm:text-7xl">
